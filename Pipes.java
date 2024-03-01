0b94d94cb7cd0fe82def20497f683b83316b8f02
Julian Farkas <julian.farkas.ger@gmail.com>
2024-03-01 22:14:02 +0100

    added pipe class

--- Pipes.java
+++ Pipes.java
@@ -1,3 +1,27 @@
+
+
 public class Pipes {
-    
+
+    public class Pipe {
+        private int height = 0;
+        private int width = 0;
+
+        public Pipe (int height, int width) {
+            this.height = height;
+            this.width = width;
+        };
+    }
+
+    private int positionX = 0;
+
+    private Pipe Top = null;
+    private Pipe Bottom = null;
+
+    public Pipes (int height, int width, int positionX) {
+
+        this.positionX = positionX;
+
+        this.Top = new Pipe ( (int)(Math.random() * height - 159), width);
+        this.Bottom = new Pipe (height - Top.height, width);
+    }    
 }
